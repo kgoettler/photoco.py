34674e459bf8a9281643c3021d2f9b75073569c6
kgoettler <38511448+kgoettler@users.noreply.github.com>
2020-11-27 20:49:45 -0800

    Finished script

--- photoco.py
+++ photoco.py
@@ -2,46 +2,77 @@
 
 from datetime import datetime
 from os.path import getmtime, exists, splitext, join
-from pprint import pprint
 import argparse
-import math
 import os
 import re
+import subprocess
+import tempfile
 
 class Photocopy:
 
     def __init__(self):
         self.parser = argparse.ArgumentParser(
-            description='Copy image files from external memory card to local disk, enforcing a date-based file structure',
-        )
-        self.parser.add_argument(
-            'start',
-            type=int,
-            help='Starting image number (e.g. 123 matches IMG_0123)',
-        )
-        self.parser.add_argument(
-            'end',
-            type=int,
-            help='Ending image number (e.g. 123 matches IMG_0123)',
+            description='Copy image files from external memory card to local disk, enforcing a date-based file structure.',
         )
         self.parser.add_argument(
             '--source',
             dest='source',
             type=str,
-            required=False,
-            default='/Volumes/Untitled/DCIM/100CANON/',
+            required=True,
             help='Source directory',
         )
         self.parser.add_argument(
             '--dest',
             dest='dest',
             type=str,
-            required=False,
-            default='/Users/kgoettler/Pictures/Canon/',
+            required=True,
             help='Destination directory',
         )
+        self.parser.add_argument(
+            '--start-index',
+            type=int,
+            help='Starting image number (e.g. 123 matches IMG_0123)',
+            required=False,
+            default=None,
+        )
+        self.parser.add_argument(
+            '--end-index',
+            type=int,
+            help='Ending image number (e.g. 123 matches IMG_0123)',
+            required=False,
+            default=None,
+        )
+        self.parser.add_argument(
+            '--start-date',
+            type=strtodate,
+            help='Start date',
+            required=False,
+            default=None,
+        )
+        self.parser.add_argument(
+            '--end-date',
+            type=strtodate,
+            help='End date',
+            required=False,
+            default=None,
+        )
+        self.parser.add_argument(
+            '-n',
+            '--dry-run',
+            dest='dryrun',
+            action='store_true',
+            required=False,
+            default=False,
+            help='Applies the --dry-run flag to rsync, thereby showing what would be transferred but not transferring any files',
+        )
+        self.parser.add_argument(
+            '-v', 
+            '--verbose',
+            action='store_true',
+            default=False,
+            help='Applies the --verbose flag to rsync, thereby printing detailed output to stdout'
+        )
         self.parser.parse_args(namespace=self)
-        print(self)
         return
 
     def __repr__(self):
@@ -51,6 +82,12 @@ class Photocopy:
         return msg
 
     def get_filelist(self):
+        '''
+        Build a dict of files to sync. 
+        
+        Each key is a destination directory, each value is a list of files to
+        copy into that directory.
+        '''
         files = {}
         for srcfile in os.listdir(self.source):
             # Parse filename
@@ -59,16 +96,24 @@ class Photocopy:
                 continue
             srcnum, ext = m.groups()
             srcnum = int(srcnum)
-            # Skip if not in range of photos
-            if ((self.start is None or srcnum < self.start) or (self.end is None or srcnum > self.end)):
-                continue
             srcpath = join(self.source, srcfile)
-            mtime = datetime.fromtimestamp(getmtime(srcpath))
+            srctime = datetime.fromtimestamp(getmtime(srcpath))
+            # Skip if not in range of photos
+            # or if not in date range
+            if (self.start_index is not None) and (srcnum < self.start_index):
+                continue
+            if (self.end_index is not None) and (srcnum > self.end_index):
+                continue
+            if (self.start_date is not None) and (self.start_date > srctime):
+                continue
+            if (self.end_date is not None) and (self.end_date < srctime):
+                continue
             outdir = join(
                 self.dest,
-                mtime.strftime('%Y'),
-                mtime.strftime('%m'),
-                mtime.strftime('%d'),
+                srctime.strftime('%Y'),
+                srctime.strftime('%m'),
+                srctime.strftime('%d'),
+                ext,
             )
             if outdir not in files:
                 files[outdir] = [srcfile]
@@ -77,11 +122,35 @@ class Photocopy:
         return files
     
     def copy(self):
+        '''
+        Perform copy
+        '''
+        rsync_flags = '-ar'
+        if self.dryrun:
+            rsync_flags += 'n'
+        if self.verbose:
+            rsync_flags += 'v'
         for dest, files in self.get_filelist().items():
-            print('Copying {} files to {}:'.format(len(files), dest))
-            for file in files:
-                print('\t{}'.format(file))
+            if not exists(dest) and not self.dryrun:
+                os.makedirs(dest)
+            print('Found {} files to sync to {}'.format(len(files), dest))
+            files_from = tempfile.NamedTemporaryFile(mode='w+', delete=False)
+            files_from.write('\n'.join(files))
+            files_from.close()
+            args = [
+                'rsync',
+                rsync_flags,
+                '--files-from={}'.format(files_from.name),
+                self.source,
+                dest,
+            ]
+            # Call
+            subprocess.call(args)
+            os.remove(files_from.name)
         return
+
+def strtodate(x):
+    return datetime.strptime(x, '%Y%m%d')
 
 if __name__ == '__main__':
     p = Photocopy()
